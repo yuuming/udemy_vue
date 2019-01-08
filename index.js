15bc3c8592ee655892cedf598bd89fdc1fe44d90
Yuumi <yuuming.n@gmail.com>
2019-01-08 23:59:59 +0900

    Add logic to check if input is empty

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var app = new Vue({
     methods: {
         addItem: function(event){
             // alert();
+            if(this.newItem == '') return;
             var todo = {
                 item: this.newItem
             };
